1e8c8085262b508a6fdb86d21f43cc38e47b461f
Gokay2705 <mstfgkcaydin@gmail.com>
2020-12-27 19:27:49 +0000

    added script for jenkins job to build and deploy app on QA environment

--- jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
+++ jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
@@ -1,7 +1,7 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
 APP_REPO_NAME="repo/petclinic-app-qa"
-APP_STACK_NAME="ppetclinic-App-QA-7"
+APP_STACK_NAME="ppetclinic-App-QA-8"
 CFN_KEYPAIR="petclinic-qa.key"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 AWS_REGION="us-east-1"
